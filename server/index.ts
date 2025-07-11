827392cd9dffcdaeed2faa3313aaa5a2cf307bfe
gshrmafp <gsharmafp@gmail.com>
2025-07-11 13:36:02 +0530

    trying to connect Mongo DB

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,20 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoUri = process.env.MONGODB_URI as string;
+
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    // Start your server here
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 const app = express();
 app.use(express.json());
